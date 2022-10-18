262c689f9206bc8c2af450379c77056bf1143f29
john-wang1015 <johnwang19931015@gmail.com>
2022-10-18 17:26:35 +1000

    update smc abc

--- SMC_ABC.py
+++ SMC_ABC.py
@@ -48,8 +48,15 @@ class SMC_ABC_method(object):
 
     def pdf(self,theta_trans):
         theta_pdf = np.prod(scipy.stats.beta.pdf(np.exp(theta_trans[0:2]/(1+ np.exp(theta_trans[0:2]))), a = [1,1], b = [1,1e5])
-                            * np.exp( ))
+                            * np.exp(theta_trans[0:2]/(1+np.exp(theta_trans[0:2])))**2, scipy.stats.norm.pdf(theta_trans[2:],0,[1,1]))
         return theta_pdf
+
+    def trans_finv(self,theta):
+        finv = np.zeros(4)
+        finv[0:2] = 1/(1 + np.exp(-theta[0:2]))
+        finv[2] = np.exp(np.log(30) + theta[2])
+        finv[3] = np.exp(np.log(160) + theta[3])
+        return finv
 
     def smc_abc_rw(self):
         part_obs = self.y
@@ -100,5 +107,34 @@ class SMC_ABC_method(object):
             sims_mcmc = np.zeros(self.N-num_keep)
 
             for i in range(num_keep+1,self.N):
-                part_vals_prop = np.random.multivariate_normal(part_vals[i],cov_matrix)
+                sum_of_dist_propr = 0
+                for r in range(mcmc_trials):
+                    part_vals_prop = np.random.multivariate_normal(part_vals[i],cov_matrix)
+                    prior_curr = self.pdf(part_vals[i])
+                    prior_prop = self.pdf(part_vals_prop)
+
+                    if (np.isnan(prior_prop/prior_curr) or np.random.rand > prior_prop/prior_curr):
+                        continue
+
+                    if sum_of_dist_propr <= dist_max:
+                        continue
+
+                    prop = self.trans_finv(part_vals_prop)
+                    part_sim_prop = Simulator(prop[0],prop[1],prop[2],prop[3],2,part_obs[0],days).Tumourgrowth()
+                    dist_prop = self.dist_function(part_obs,part_sim_prop)
+                    sum_of_dist_propr = sum_of_dist_propr + dist_prop
+
+                    sims_mcmc[i-num_keep] = sims_mcmc[i-num_keep]+1
+
+                    if dist_prop <= dist_next:
+                        part_vals[i] = part_vals_prop
+                        part_s[i] = dist_prop
+                        part_sim[i] = part_sim_prop
+                        i_acc[i-num_keep] = i_acc[i-num_keep] + 1
+
+            acc_rate = np.sum(i_acc)/(mcmc_trials*(self.N-num_keep))
+            mcmc_iters = np.floor(np.log(self.c)/np.log(1-acc_rate)+1)
+            print("Total number of mcmc moves for current target is {:d}, number remaining is {:d}".format(mcmc_iters,mcmc_iters-mcmc_trials))
+
+
 
